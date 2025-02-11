fdfea6ddf2f1f212afc7d268238b0e69f4b80e5b
Ty Qualters <qualtersty@gmail.com>
2025-02-11 13:39:26 -0600

    bruh

---
+++ scripts/sigscan.lua
@@ -0,0 +1,11 @@
+---
+--- Created by Ty Qualters.
+--- DateTime: 2/11/2025 1:33 PM
+---
+
+-- TODO: Finish implementing this
+
+if #arguments < 2 then
+    print("Requires parameters [Binary File] [Pattern] [Mask]")
+    print("Example: test.exe \\x55\\??\\xEC\\x51\\x53\\x56\\x57 xx??xxxxxxxxxx")
+end
